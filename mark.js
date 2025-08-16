c0079da1d279452145da8679455aa46d99e8c546
Mahidi-Hasan-Supon <mahidihasansupon@gmail.com>
2025-08-16 20:08:08 +0600

    added

--- mark.js
+++ mark.js
@@ -21,3 +21,36 @@ function number(num1,num2){
         return num2;
     }
 }
+
+const num = number(20,10)
+console.log(num)
+
+
+const jim = 50;
+const rim = 60;
+const kim = 70;
+if(jim>kim && jim >rim){
+    console.log("jim the boss")
+}
+else if(rim>kim && rim>jim){
+    console.log("rim the boss")
+}
+else{
+    console.log("kim the boss")
+}
+
+// function diye
+
+function numbers(number1,number2,number3){
+    if(number1>number2 && number1>number3){
+        return number1;
+    }
+    else if(number2>number1 && number2>number3){
+        return number2;
+    }else{
+        return number3
+    }
+}
+
+const result = numbers(20,50,80);
+console.log(result)
---
+++ tallestNumber.js
@@ -0,0 +1,33 @@
+// const number5 = [45,54,875,9641,458];
+
+// function maxNumber(numbers){
+//     let max = numbers[0];
+//     for(const num of numbers){
+//         if(num > max){
+//             max = num;
+//         }
+//     }
+//     return max
+// }
+
+// const maxNumber1 = maxNumber(number5)
+
+// console.log(maxNumber1)
+
+
+// smallest  number in function
+const number5 = [45,54,875,9641,458];
+
+function minNumber(numbers){
+    let min= numbers[0];
+    for(const num of numbers){
+        if(num < min){
+            min = num;
+        }
+    }
+    return min
+}
+
+const minNumber1 = minNumber(number5)
+
+console.log(minNumber1)
