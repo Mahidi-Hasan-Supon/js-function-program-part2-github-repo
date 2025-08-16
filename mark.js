02ac486567f781d9ee677ee6e55925da00d70384
Mahidi-Hasan-Supon <mahidihasansupon@gmail.com>
2025-08-16 18:33:21 +0600

    first commit

---
+++ mark.js
@@ -0,0 +1,23 @@
+// je besi number paibe she puruskar pabe<<>>>>>
+const myNumber = 50;
+const friendsNumber = 70;
+
+if(myNumber > friendsNumber){
+    console.log("you got a strawbary");
+
+}
+
+else{
+    console.log("you are loser")
+}
+
+
+// function realitat
+
+function number(num1,num2){
+    if(num1>num2){
+        return num1;
+    }else{
+        return num2;
+    }
+}
